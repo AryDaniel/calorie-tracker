3b3b759fecc92569467325fc0d665af8ef8b7cda
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-04-23 00:12:35 -0700

    feat(form): integrate activity form with reducer and dispatch actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { useReducer } from "react"
 import Form from "./components/Form"
-
+import { activityReducer, initialState } from "./reducer/activityReducer"
 function App() {
 
+  const [state, dispatch] = useReducer(activityReducer, initialState)
   return (
     <>
       <header className="bg-lime-600 py-3">
@@ -16,7 +18,9 @@ function App() {
 
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form />
+          <Form 
+            dispatch={dispatch}
+          />
         </div>
       </section>
     </>
