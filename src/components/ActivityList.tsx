d8a9c6250a8ef5f91da3b5e70bca962f81b3cdfa
AryDaniel <ary.lopez@cetis107.edu.mx>
2025-05-13 22:06:15 -0700

    refactor(form): refactor Form component and use ActivityContext

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -14,7 +14,7 @@ export default function ActivityList() {
             {isEmptyActivities ? <p className="text-center my-5">No hay actividates</p> : 
             
             state.activities.map( activity => (
-                <div key={activity.id} className="px-5 py-10 bg-white mt- flex justify-between shadow">
+                <div key={activity.id} className="px-5 py-5 bg-white mt-5 flex justify-between shadow">
                     <div className="space-y-2 relative">
                         <p className={`absolute -top-8 left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
                             {categoryName(+activity.category)}
